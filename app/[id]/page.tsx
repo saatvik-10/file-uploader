7173150cdbb8f8bfc9eb3a2cde2a7e086044098b
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-08-27 02:31:02 +0530

    completed ui

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -1,6 +1,67 @@
 import { PhotoIcon } from '@heroicons/react/24/outline'
+import { Download, File } from 'lucide-react'
 import React from 'react'
 
+const people = [
+    {
+        name: 'Leslie Alexander',
+        email: 'leslie.alexander@example.com',
+        role: 'Co-Founder / CEO',
+        imageUrl:
+            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        href: '#',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Michael Foster',
+        email: 'michael.foster@example.com',
+        role: 'Co-Founder / CTO',
+        imageUrl:
+            'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        href: '#',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Dries Vincent',
+        email: 'dries.vincent@example.com',
+        role: 'Business Relations',
+        imageUrl:
+            'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        href: '#',
+        lastSeen: null,
+    },
+    {
+        name: 'Lindsay Walton',
+        email: 'lindsay.walton@example.com',
+        role: 'Front-end Developer',
+        imageUrl:
+            'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        href: '#',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Courtney Henry',
+        email: 'courtney.henry@example.com',
+        role: 'Designer',
+        imageUrl:
+            'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        href: '#',
+        lastSeen: '3h ago',
+        lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+        name: 'Tom Cook',
+        email: 'tom.cook@example.com',
+        role: 'Director of Product',
+        imageUrl:
+            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+        href: '#',
+        lastSeen: null,
+    },
+]
 const Dashboard = () => {
     return (
         <div>
@@ -36,6 +97,44 @@ const Dashboard = () => {
                     />
                 </label>
             </div>
+            <ul
+                role="list"
+                className="divide-y mt-10 divide-gray-100 overflow-hidden bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl"
+            >
+                {people.map((person) => (
+                    <li
+                        key={person.email}
+                        className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6"
+                    >
+                        <div className="flex min-w-0 gap-x-4 items-center">
+                            <File
+                                className="h-10 flex-none "
+                                aria-hidden="true"
+                            />
+                            <div className="min-w-0 flex-auto">
+                                <p className="text-sm font-semibold leading-6 text-gray-900">
+                                    <span className="absolute inset-x-0 -top-px bottom-0" />
+                                    {person.name}
+                                </p>
+                            </div>
+                        </div>
+                        <div className="flex shrink-0 items-center gap-x-4">
+                            <div className="hidden sm:flex sm:flex-col sm:items-end">
+                                <p className="mt-1 text-xs leading-5 text-gray-500">
+                                    Last seen
+                                    <time dateTime={person.lastSeenDateTime}>
+                                        {person.lastSeen}
+                                    </time>
+                                </p>
+                            </div>
+                            <Download
+                                className="h-5 w-5 flex-none text-gray-400"
+                                aria-hidden="true"
+                            />
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
